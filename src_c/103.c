2fbb18e4f2341e00526b7a2e285e6d1f652cc833
ceba <ceba_robot@outlook.com>
2021-09-11 20:21:54 +0800

    feat: support basic arithmetic inst

--- src_c/103.c
+++ src_c/103.c
@@ -7,6 +7,8 @@
 #include "103.h"
 #include "tape.h"
 
+FILE * typewriter;
+
 num_t * machine_mem;
 num_t reg_addr;
 
@@ -90,8 +92,183 @@ void print_mem() {
 
 void init_machine() {
     init_mem();
+    init_output();
 }
 
 void run_machine() {
+    num_t op, c1, c2;
+    num_t op1, op2;
+    while (1) {
+        // 1
+        reg_addr = reg_pc;
+        
+        // 2
+        reg_c = machine_mem[reg_addr];
+        op = (reg_c >> 24) & 0x3f;
+        op1 = op >> 3;
+        op2 = op & 0x7;
+        c1 = (reg_c >> 12) & 0xfff;
+        c2 = (reg_c) & 0xfff;
+        
+        if (op2 == OP_ADD || op2 == OP_SUB || op2 == OP_DIV || op2 == OP_MUL || op2 == OP_AND) {
+            reg_addr = c1;
+            reg_c = machine_mem[reg_addr];
+            reg_a = reg_c;
 
+            reg_addr = c2;
+            if (op1 == 0 || op1 == 1 || op1 == 4 || op1 == 5) {
+                reg_c = machine_mem[reg_addr];
+                reg_b = reg_c;
+            }
+
+            int sign_a, sign_b;
+            if (op == 5 || op == 7) {
+                sign_a = 1;
+                sign_b = 1;
+            } else {
+                sign_a = (reg_a & 0x40000000) ? -1 : 1;
+                sign_b = (reg_b & 0x40000000) ? -1 : 1;
+            }
+
+            if (op2 == OP_ADD) {
+                
+            } else if (op2 == OP_SUB) {
+
+            } else if (op2 == OP_DIV) {
+
+            } else if (op2 == OP_MUL) {
+
+            } else if (op2 == OP_AND) {
+
+            }
+
+            reg_b = reg_c;
+
+            if (op2 == 0 || op2 == 2 || op == 4 || op == 6) {
+                machine_mem[reg_addr] = reg_c;
+            }
+            if (op == 4 || op == 6) {
+                output_reg_c();
+            }
+
+        // } else if (op == 007 || op == 027) {
+
+        // } else if (op == 005 || op == 015) {
+
+        // } else if (op == 045 || op == 055) {
+
+        // } else if (op == 024) {
+
+        // } else if (op == 064) {
+
+        // } else if (op == 074) {
+
+        // } else if (op == 034) {
+
+        // } else if (op == 004 || op == 014 || op == 044) {
+
+        // } else if (op == 054) {
+            
+        // } else if (op == 037) {
+
+        // } else if (op == 057) {
+
+        // } else if (op == 077) {
+
+        } else {
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void init_output() {
+    typewriter = fopen("103-output.txt", "w");
+}
+
+void output_reg_c() {
+    fprintf(typewriter, "%c ", (reg_c & 0x40000000) ? '-' : '+');
+    if (switch_output_numeral == DECIMAL) {
+        for (int i = 26; i >= 0; i -= 4) {
+            fprintf(typewriter, "%d", (reg_c >> i) & 0xF);
+        }
+    } else if (switch_output_numeral == OCTAL) {
+        for (int i = 27; i >= 0; i -= 3) {
+            fprintf(typewriter, "%d", (reg_c >> i) & 0x7);
+        }
+    }
+    fprintf(typewriter, "\n");
+}
+
+num_t do_add(int sign_a, int num_a, int sign_b, int num_b) {
+    int res = (sign_b * num_b) + (sign_a * num_a);
+    num_t ans;
+    if (res > 0x3fffffff || res < -0x3fffffff) {
+        fprintf(stderr, "overflow\n");
+        exit(EXIT_FAILURE);
+    }
+    if (res > 0) {
+        ans = res & 0x3fffffff;
+    } else if (res < 0) {
+        ans = ((-res) & 0x3fffffff) | 0x40000000;
+    } else {
+        ans = (sign_b < 0) << 30;
+    }
+    return ans;
+}
+
+num_t do_sub(int sign_a, int num_a, int sign_b, int num_b) {
+    int res = (sign_b * num_b) - (sign_a * num_a);
+    num_t ans;
+    if (res > 0x3fffffff || res < -0x3fffffff) {
+        fprintf(stderr, "overflow\n");
+        exit(EXIT_FAILURE);
+    }
+    if (res > 0) {
+        ans = res & 0x3fffffff;
+    } else if (res < 0) {
+        ans = ((-res) & 0x3fffffff) | 0x40000000;
+    } else {
+        ans = (sign_b < 0) << 30;
+    }
+    return ans;
+}
+
+num_t do_div(int sign_a, long num_a, int sign_b, long num_b) {
+    long res = ((num_b << 30) / num_a);
+    num_t ans;
+    if (res > 0x3fffffff) {
+        fprintf(stderr, "overflow\n");
+        exit(EXIT_FAILURE);
+    }
+    if (sign_a * sign_b > 0) {
+        ans = res & 0x3fffffff;
+    } else if (sign_a * sign_b < 0) {
+        ans = (res & 0x3fffffff) | 0x40000000;
+    } else {
+        ans = (sign_b < 0) << 30;
+    }
+    return ans;
+}
+
+num_t do_mul(int sign_a, long num_a, int sign_b, long num_b) {
+    long res = (num_b * num_a) >> 30;
+    num_t ans;
+    if (res > 0x3fffffff) {
+        fprintf(stderr, "overflow\n");
+        exit(EXIT_FAILURE);
+    }
+    if (sign_a * sign_b > 0) {
+        ans = res & 0x3fffffff;
+    } else if (sign_a * sign_b < 0) {
+        ans = (res & 0x3fffffff) | 0x40000000;
+    } else {
+        ans = (sign_b < 0) << 30;
+    }
+    return ans;
+}
+
+num_t do_and(int sign_a, int num_a, int sign_b, int num_b) {
+    int res = num_b & num_a;
+    num_t ans = res | ((sign_b < 0) << 30);
+    return ans;
 }
--- src_c/103.h
+++ src_c/103.h
@@ -1,6 +1,12 @@
 #define DECIMAL 1
 #define OCTAL   0
 
+#define OP_ADD  0
+#define OP_SUB  1
+#define OP_DIV  2
+#define OP_MUL  3
+#define OP_AND  6
+
 typedef unsigned int num_t;
 
 void set_pc(int val);
@@ -15,3 +21,12 @@ void mem_read();
 
 void init_machine();
 void run_machine();
+
+void init_output();
+void output_reg_c();
+
+num_t do_add(int sign_a, int num_a, int sign_b, int num_b);
+num_t do_sub(int sign_a, int num_a, int sign_b, int num_b);
+num_t do_div(int sign_a, long num_a, int sign_b, long num_b);
+num_t do_mul(int sign_a, long num_a, int sign_b, long num_b);
+num_t do_and(int sign_a, int num_a, int sign_b, int num_b);
